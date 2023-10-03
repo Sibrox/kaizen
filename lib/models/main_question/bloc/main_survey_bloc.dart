edf83a6faa6a88c326b829998dba4e243839a3a4
Stefano Sidoni <oniryu95@yahoo.it>
2023-10-03 17:09:17 +0200

    Change the main_question_bloc to main_survey_bloc
    Signed-off-by: Stefano Sidoni <oniryu95@yahoo.it>

---
+++ lib/models/main_question/bloc/main_survey_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:kaizen/models/main_question/main_question.dart';
+
+part 'main_question_event.dart';
+part 'main_question_state.dart';
+
+class MainSurveyBloc extends Bloc<MainQuestionEvent, MainQuestionState> {
+
+  MainSurveyBloc(List<MainQuestion> mainQuestions) : super(MainQuestionState(mainQuestions)) {
+
+    on<EventToggleMainQuestion>((event, emit) {
+      List<MainQuestion> newState = List.from(state.mainQuestions);
+      newState[event.index].value = !newState[event.index].value!;
+      emit(MainQuestionState(newState));
+    });
+  }
+
+}
