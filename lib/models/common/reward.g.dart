8413a39d51b364ef08b8f3aba3a46f1205bf0f8a
Davide Gallotti <33552039+gallottino@users.noreply.github.com>
2023-10-04 11:25:14 +0200

    feat(#15): add `Reward` model (#58)

---
+++ lib/models/common/reward.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../common/reward.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Reward _$RewardFromJson(Map<String, dynamic> json) => Reward(
+      title: json['title'] as String,
+      description: json['description'] as String,
+      price: (json['price'] as num).toInt(),
+      rating: json['rating'] as int,
+    );
+
+Map<String, dynamic> _$RewardToJson(Reward instance) => <String, dynamic>{
+      'title': instance.title,
+      'description': instance.description,
+      'price': instance.price,
+      'rating': instance.rating,
+    };
---
+++ test/models/rewards/reward_serde.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:kaizen/models/common/reward.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Test reward serialization', () {
+    const strReward = '''
+      {
+        "title": "Reward",
+        "description": "Description",
+        "price": 100,
+        "rating": 2
+      }
+    ''';
+
+    Map<String, dynamic> jsonReward = jsonDecode(strReward);
+    var reward = Reward.fromJson(jsonReward);
+
+    assert(reward.title == "Reward");
+    assert(reward.description == "Description");
+    assert(reward.price == 100);
+    assert(reward.rating == 2);
+  });
+
+  test('Test reward deserialization', () {
+    const reward = Reward(
+        title: "Reward", description: "Description", price: 100, rating: 2);
+    var jsonReward = reward.toJson();
+
+    assert(jsonReward["title"] == "Reward");
+    assert(reward.description == "Description");
+    assert(reward.price == 100);
+    assert(reward.rating == 2);
+  });
+}
