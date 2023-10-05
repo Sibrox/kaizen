3ad8a640899d446f5e4e6bae32197c4f47172da4
Stefano Sidoni <oniryu95@yahoo.it>
2023-10-05 14:23:28 +0200

    feat: complete MainQuestionWidget and MainSurveyScreen
    Signed-off-by: Stefano Sidoni <oniryu95@yahoo.it>

--- lib/models/main_question/bloc/main_question_event.dart
+++ lib/models/main_question/bloc/main_question_event.dart
@@ -1,14 +1,17 @@
 part of 'main_survey_bloc.dart';
 
 abstract class MainQuestionEvent {
-
   const MainQuestionEvent();
-
 }
 
-class EventToggleMainQuestion extends MainQuestionEvent{
-
+class EventToggleMainQuestion extends MainQuestionEvent {
   int index;
 
   EventToggleMainQuestion(this.index);
+}
+
+class EventAddQuestion extends MainQuestionEvent {
+  String text;
+
+  EventAddQuestion(this.text);
 }
--- lib/screens/main_survey.dart
+++ lib/screens/main_survey.dart
@@ -1,32 +1,52 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kaizen/models/main_question/bloc/main_survey_bloc.dart';
-import 'package:kaizen/models/main_question/main_question.dart';
+import 'package:kaizen/models/main_question/view/main_question_widget.dart';
+import 'package:blur/blur.dart';
 
 class MainSurveyWidget extends StatelessWidget {
-
-  MainSurveyWidget({super.key});
+  const MainSurveyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  BlocBuilder<MainSurveyBloc,MainQuestionState>(
-      builder: (context,state){
-        return ListView.builder(
-          itemCount: state.mainQuestions.length,
-          itemBuilder: (BuildContext context, int index){
-            return Row(
-              children: [
-                Text("${state.mainQuestions[index].text}"),
-                GestureDetector(
-                  child: Text("${state.mainQuestions[index].value}"),
-                  onTap: (){
-
-                    context.read<MainSurveyBloc>().add(EventToggleMainQuestion(index));
-                  },
-                )
-              ],
-            );
-          },
+    return BlocBuilder<MainSurveyBloc, MainQuestionState>(
+      builder: (context, state) {
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                fit: BoxFit.fill,
+                image:
+                    ExactAssetImage("assets/images/main_survey_background.jpg"),
+              )),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                ),
+              ),
+            ),
+            ListView.separated(
+              padding: const EdgeInsetsDirectional.all(30),
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: state.mainQuestions.length,
+              itemBuilder: (BuildContext context, int index) {
+                return MainQuestionWidget(
+                  index: index,
+                  question: state.mainQuestions[index],
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+            ),
+          ],
         );
       },
     );
