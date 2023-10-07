e0a5b40ed233f0986e8165c205650f02f6274239
Eugenio Lo Grasso <eugenio.lo.grasso@gmail.com>
2023-10-07 13:35:04 +0200

    feat(#44): add shop model (#60)

--- lib/models/shop/shop.dart
+++ lib/models/shop/shop.dart
@@ -1,1 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:kaizen/models/common/reward.dart';
+part 'shop.g.dart';
 
+@JsonSerializable()
+class Shop {
+  final Reward milestone;
+  final List<Reward> rewards;
+
+  const Shop({
+    required this.milestone,
+    required this.rewards,
+  });
+
+  factory Shop.fromJson(Map<String, dynamic> json) => _$ShopFromJson(json);
+  Map<String, dynamic> toJson() => _$ShopToJson(this);
+}
---
+++ lib/models/shop/shop.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shop.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Shop _$ShopFromJson(Map<String, dynamic> json) => Shop(
+      milestone: Reward.fromJson(json['milestone'] as Map<String, dynamic>),
+      rewards: (json['rewards'] as List<dynamic>)
+          .map((e) => Reward.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$ShopToJson(Shop instance) => <String, dynamic>{
+      'milestone': instance.milestone,
+      'rewards': instance.rewards,
+    };
