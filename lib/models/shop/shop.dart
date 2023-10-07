4032a36aabe25b1e0f7cea5f1a30eeec4859af91
Davide Gallotti <d.gallotti@outlook.it>
2023-10-07 13:37:54 +0200

    fix: import reward

--- lib/models/shop/shop.dart
+++ lib/models/shop/shop.dart
@@ -1,5 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
-import 'package:kaizen/models/common/reward.dart';
+import 'package:kaizen/models/common/reward/reward.dart';
 part 'shop.g.dart';
 
 @JsonSerializable()
