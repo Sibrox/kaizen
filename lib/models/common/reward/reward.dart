3cc6a4b5a40a56b84e2cad794a0b8c758b1c4e7e
Davide Gallotti <d.gallotti@outlook.it>
2023-10-07 12:37:30 +0200

    refactor: reward model

--- lib/models/common/reward/reward.dart
+++ lib/models/common/reward/reward.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 part 'reward.g.dart';
 
@@ -13,11 +15,28 @@ class Reward {
 
   const Reward(
       {required this.title,
-      required this.description,
       required this.price,
-      required this.rating});
+      required this.rating,
+      this.description = ""});
 
   factory Reward.fromJson(Map<String, dynamic> json) => _$RewardFromJson(json);
 
   Map<String, dynamic> toJson() => _$RewardToJson(this);
+
+  @override
+  bool operator ==(Object other) {
+    return other is Reward &&
+        title == other.title &&
+        price == other.price &&
+        rating == other.rating &&
+        description == other.description;
+  }
+
+  @override
+  int get hashCode => title.hashCode;
+
+  @override
+  String toString() {
+    return const JsonEncoder.withIndent('  ').convert(toJson());
+  }
 }
