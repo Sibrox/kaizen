629b99c2363afb5ff653ba0f0d82c8ea2552094f
Davide Gallotti <d.gallotti@outlook.it>
2023-10-16 18:26:54 +0200

    feat: RewardForm uses NewRewardEvent from ShopBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kaizen/models/reward/view/reward_form_button.dart';
 import 'package:kaizen/models/second_survey/bloc/second_survey_bloc.dart';
+import 'package:kaizen/models/shop/bloc/shop_bloc.dart';
 import 'package:kaizen/screens/waiting_screen.dart';
 import 'package:kaizen/screens/main_survey.dart';
 import 'models/main_survey/bloc/main_survey_bloc.dart';
@@ -21,25 +23,25 @@ class Kaizen extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: MultiBlocProvider(
-            providers: [
-              BlocProvider<MainSurveyBloc>(
-                  create: (context) => MainSurveyBloc()
-                    ..add(EventLoadInfos(
-                        "assets/jsons/ita/main_questions.json"))),
-              BlocProvider<SecondSurveyBloc>(
-                  lazy: false,
-                  create: (context) => SecondSurveyBloc()
-                    ..add(EventLoadSecondSurvey(
-                        "assets/jsons/ita/second_questions.json"))),
-            ],
-            child: WaitingScreen<MainSurveyBloc, MainSurveyState>(
-              builder: (state) => MainSurveyWidget(
-                mainQuestions: state.mainQuestions,
-              ),
-              buildWhen: (state) => state.mainQuestions.isNotEmpty,
-            )),
-      ),
+          body: MultiBlocProvider(
+              providers: [
+            BlocProvider<MainSurveyBloc>(
+                create: (context) => MainSurveyBloc()
+                  ..add(
+                      EventLoadInfos("assets/jsons/ita/main_questions.json"))),
+            BlocProvider<SecondSurveyBloc>(
+                lazy: false,
+                create: (context) => SecondSurveyBloc()
+                  ..add(EventLoadSecondSurvey(
+                      "assets/jsons/ita/second_questions.json"))),
+            BlocProvider<ShopBloc>(lazy: false, create: (context) => ShopBloc())
+          ],
+              child: WaitingScreen<MainSurveyBloc, MainSurveyState>(
+                builder: (state) => MainSurveyWidget(
+                  mainQuestions: state.mainQuestions,
+                ),
+                buildWhen: (state) => state.mainQuestions.isNotEmpty,
+              ))),
     );
   }
 }
--- lib/models/reward/view/reward_form_button.dart
+++ lib/models/reward/view/reward_form_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kaizen/models/reward/view/reward_form.dart';
+import 'package:kaizen/models/shop/bloc/shop_bloc.dart';
 
 class RewardFormButton extends StatelessWidget {
   const RewardFormButton({super.key});
@@ -7,12 +9,14 @@ class RewardFormButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-                  child: const Text("Show Reward Form"),
-                  onPressed: () {
-                    showBottomSheet(
-                        context: context,
-                        enableDrag: false,
-                        builder: (BuildContext context) => const RewardForm());
-                  });
+        child: const Text("Show Reward Form"),
+        onPressed: () {
+          showBottomSheet(
+              context: context,
+              builder: (BuildContext newContext) => BlocProvider.value(
+                    value: BlocProvider.of<ShopBloc>(context),
+                    child: const RewardForm(),
+                  ));
+        });
   }
 }
