d89fde24d4bc6974a4c71948ce4584e2823f4b26
Stefano Sidoni <oniryu95@yahoo.it>
2023-10-17 17:34:31 +0200

    Add ScreenWidget

--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kaizen/models/common/reward/view/milestone_widget.dart';
+import 'package:kaizen/models/common/reward/view/reward_widget.dart';
 import 'package:kaizen/models/user/bloc/user_bloc.dart';
-
 import '../models/shop/bloc/shop_bloc.dart';
 import '../models/shop/shop.dart';
 
@@ -18,10 +18,23 @@ class ShopScreen extends StatelessWidget {
 
         return Column(
           children: [
-            shopBloc.state.milestone != null
-                ? MilestoneWidget(
-                    milestone: shopBloc.state.milestone!, user: userBloc.state)
-                : Container(),
+            Expanded(
+              flex: 2,
+              child: shopBloc.state.milestone != null
+                  ? MilestoneWidget(
+                      milestone: shopBloc.state.milestone!,
+                      user: userBloc.state)
+                  : const Text("Drag here a reward to set a milestone!"),
+            ),
+            Expanded(
+              flex: 4,
+              child: ListView.builder(
+                itemCount: shopBloc.state.rewards.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return RewardWidget(reward: shopBloc.state.rewards[index]);
+                },
+              ),
+            ),
           ],
         );
       },
