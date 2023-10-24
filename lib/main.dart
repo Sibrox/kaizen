4ab15c540af5c204a319c0281df226d9a9dced3c
Stefano Sidoni <oniryu95@yahoo.it>
2023-10-24 11:44:38 +0200

    Implement draggable add milestone

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kaizen/models/second_survey/bloc/second_survey_bloc.dart';
 import 'package:kaizen/models/shop/bloc/shop_bloc.dart';
-import 'package:kaizen/screens/waiting_screen.dart';
 import 'package:kaizen/screens/main_survey.dart';
+import 'package:kaizen/screens/waiting_screen.dart';
 import 'models/main_survey/bloc/main_survey_bloc.dart';
 
 void main() {
@@ -33,7 +33,8 @@ class Kaizen extends StatelessWidget {
                 create: (context) => SecondSurveyBloc()
                   ..add(EventLoadSecondSurvey(
                       "assets/jsons/ita/second_questions.json"))),
-            BlocProvider<ShopBloc>(lazy: false, create: (context) => ShopBloc())
+            BlocProvider<ShopBloc>(
+                lazy: false, create: (context) => ShopBloc()),
           ],
               child: WaitingScreen<MainSurveyBloc, MainSurveyState>(
                 builder: (state) => MainSurveyWidget(
--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -6,8 +6,22 @@ import 'package:kaizen/models/user/bloc/user_bloc.dart';
 import '../models/shop/bloc/shop_bloc.dart';
 import '../models/shop/shop.dart';
 
-class ShopScreen extends StatelessWidget {
+class ShopScreen extends StatefulWidget {
   const ShopScreen({super.key});
+
+  @override
+  State<ShopScreen> createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  late bool isDragging;
+  late bool inMilestone;
+
+  @override
+  void initState() {
+    isDragging = inMilestone = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +34,73 @@ class ShopScreen extends StatelessWidget {
           children: [
             Expanded(
               flex: 2,
-              child: shopBloc.state.milestone != null
-                  ? MilestoneWidget(
-                      milestone: shopBloc.state.milestone!,
-                      user: userBloc.state)
-                  : const Text("Drag here a reward to set a milestone!"),
+              child: DragTarget<int>(
+                builder: (BuildContext context, List<dynamic> accepted,
+                    List<dynamic> rejected) {
+                  return shopBloc.state.milestone == null || isDragging
+                      ? AnimatedContainer(
+                          color: inMilestone ? Colors.blueAccent : Colors.white,
+                          duration: const Duration(milliseconds: 500),
+                          child: const Center(
+                            child:
+                                Text("Drag here a reward to set a milestone!"),
+                          ))
+                      : MilestoneWidget(
+                          milestone: shopBloc.state.milestone!,
+                          user: userBloc.state);
+                },
+                onAccept: (int index) {
+                  shopBloc.add(SetMilestoneEvent(index));
+                },
+                onMove: (details) {
+                  setState(() {
+                    inMilestone = true;
+                  });
+                },
+                onLeave: (details) {
+                  setState(() {
+                    inMilestone = false;
+                  });
+                },
+              ),
             ),
             Expanded(
               flex: 4,
               child: ListView.builder(
+                padding: EdgeInsets.zero,
                 itemCount: shopBloc.state.rewards.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return RewardWidget(reward: shopBloc.state.rewards[index]);
+                  return LongPressDraggable<int>(
+                      onDragStarted: () {
+                        setState(() {
+                          isDragging = true;
+                        });
+                      },
+                      onDragEnd: (dragDettails) {
+                        setState(() {
+                          isDragging = inMilestone = false;
+                        });
+                      },
+                      dragAnchorStrategy: pointerDragAnchorStrategy,
+                      data: index,
+                      childWhenDragging: ColorFiltered(
+                        colorFilter: const ColorFilter.mode(
+                          Colors.grey,
+                          BlendMode.saturation,
+                        ),
+                        child:
+                            RewardWidget(reward: shopBloc.state.rewards[index]),
+                      ),
+                      feedback: const SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Icon(Icons.add_box_rounded),
+                        ),
+                      ),
+                      child:
+                          RewardWidget(reward: shopBloc.state.rewards[index]));
                 },
               ),
             ),
