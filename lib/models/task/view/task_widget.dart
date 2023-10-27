1763d5573358dbbbb919d61dd660833db2ae19ef
Hugene <eugenio.lo.grasso@gmail.com>
2023-10-27 15:54:08 +0200

    feat(#71): create task widget

---
+++ lib/models/task/view/task_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+import 'package:kaizen/models/task/task.dart';
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      color: const Color.fromARGB(255, 255, 145, 56),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(task.title),
+              Text(task.description),
+              Text(task.routine.toString()),
+              Text('${task.taskCredits}')
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
